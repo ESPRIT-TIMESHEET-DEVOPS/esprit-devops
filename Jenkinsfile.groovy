0ce16c412aa7a3a48cd44112a7332f0b6a052b92
chihabhajji <35853327+chihabhajji@users.noreply.github.com>
2021-11-11 00:41:16 +0100

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,12 +1,29 @@
 node {
 //    OK
-    stage('SCM') {
-        checkout scm
+
+}
+pipeline {
+    agent any
+    tools { 
+        maven 'Maven 3.3.9' 
     }
-    stage('SonarQube Analysis') {
-        def mvn = tool 'Default Maven';
-        withSonarQubeEnv() {
-            bat "${mvn}/bin/mvn clean verify sonar:sonar"
+    stages {
+        stage ('Initialize') {
+            steps {
+                bat echo "M2_HOME = ${M2_HOME}"
+            }
+        }
+        stage('SCM') {
+            checkout scm
+        }
+            withSonarQubeEnv() {
+                bat "mvn clean verify sonar:sonar"
+            }
+        }
+        stage ('Build') {
+            steps {
+                echo 'This is a minimal pipeline.'
+            }
         }
     }
 }
