0ca36839027daeae2e08a55bb082df765d7e0b4a
chihabhajji <blackpang23@gmail.com>
2021-11-11 09:58:29 +0100

    ok

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -22,10 +22,9 @@ pipeline {
             }
         }
         stage('SonarQube Analysis') {
-            def mvn = tool 'Default Maven';
             steps{
                 withSonarQubeEnv('Default SonarQube') {
-                    sh "${mvn}/bin/mvn clean verify org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar -DskipTests"
+                    sh "mvn clean verify org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar -DskipTests"
                 }
             }
         }
