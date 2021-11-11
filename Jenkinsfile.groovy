4718ff434796c3fd71a54f00e392b880bb9ce138
chihabhajji <35853327+chihabhajji@users.noreply.github.com>
2021-11-11 02:49:47 +0100

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -19,11 +19,16 @@ node {
     withSonarQubeEnv() {
       sh "${mvn}/bin/mvn clean verify sonar:sonar -DskipTests"
     }
+  }
+  stage('Build docker image') {
+    withMaven {
+        sh "${mvn}/bin/mvn spring-boot:build-image"
+    }
     post {
-        always {
-            deleteDir() /* clean up our workspace */
-        }
-        success {
+      always {
+        deleteDir() /* clean up our workspace */
+       }
+       success {
              mail to: 'chihab.hajji@esprit.tn',
              subject: "Successful Pipeline: ${currentBuild.fullDisplayName}",
              body: "Pipeline ${env.BUILD_URL} built with success!"
@@ -33,11 +38,6 @@ node {
              subject: "Failed Pipeline: ${currentBuild.fullDisplayName}",
              body: "Something is wrong with ${env.BUILD_URL}'s SonarQubeAnalysis"
         }
-    }
-  }
-  stage('Build docker image') {
-    withMaven {
-        sh "${mvn}/bin/mvn spring-boot:build-image"
     }
   }
 }
