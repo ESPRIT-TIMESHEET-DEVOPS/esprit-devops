9071119c5383c2f0f2e3bb3e95496f4f672f3666
chihabhajji <blackpang23@gmail.com>
2021-11-11 13:11:15 +0100

    ok

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -18,50 +18,7 @@ pipeline {
                 }
             }
         }
-        stage('SonarQube Analysis') {
-            steps{
-                withSonarQubeEnv('Default SonarQube') {
-                    sh "mvn clean verify org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar -DskipTests"
-                }
-            }
-        }
-        stage('Local Integration Tests') {
-            steps{
-                withMaven{
-                    sh "mvn -B org.jacoco:jacoco-maven-plugin:prepare-agent-integration failsafe:integration-test failsafe:verify -DskipTests"
-                }
-//                step([$class: 'JUnitResultArchiver', testResults: '**/target/failsafe-reports/TEST-*.xml'])
-            }
-        }
-        stage('Build'){
-            steps{
-                sh "mvn clean install -DskipTests"
-            }
-        }
-        stage('Build docker image') {
-            steps {
-                sh "docker build -t espritchihab/${IMAGE}:${VERSION} ."
-            }
-        }
-        stage('Login to docker') {
-            steps {
-                sh 'echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin'
-            }
-        }
-        stage('Push to docker') {
-            steps {
-                sh "docker push espritchihab/${IMAGE}:${VERSION}"
-            }
-        }
-        stage('Deploy to Nexus') {
-            steps{
-//                            withMaven {
-//                sh "mvn install deploy -DskipTests"
-//                rchiveArtifacts artifacts: '**/timesheet-*.jar', onlyIfSuccessful: false
-//            }
-                sh 'echo done'
-            }
-        }
+
     }
     post {
         always {
@@ -72,6 +29,12 @@ pipeline {
             mail to: 'chihab.hajji@esprit.tn',
                     subject: "Failed Pipeline: ${currentBuild.fullDisplayName}",
                     body: "Something is wrong with ${env.BUILD_URL}'s test"
+            success{
+                discordSend description: "${IMAGE}:${VERSION} Pipeline Build", footer: "Build successful", link: env.BUILD_URL, result: currentBuild.currentResult, title: JOB_NAME, webhookURL: "https://discord.com/api/webhooks/908327603428028447/WqNlqvRQhP2caIzVFKOoItlXZa7yJIXiQUVjrKIfGRfhU_W184n_zfm2uZGQZOeE1Oba"
+            }
+        }
+        success{
+            discordSend description: "${IMAGE}:${VERSION} Pipeline Build", footer: "Build successful", link: env.BUILD_URL, result: currentBuild.currentResult, title: JOB_NAME, webhookURL: "https://discord.com/api/webhooks/908327603428028447/WqNlqvRQhP2caIzVFKOoItlXZa7yJIXiQUVjrKIfGRfhU_W184n_zfm2uZGQZOeE1Oba"
         }
     }
 }
