0c0b0bad7cb3865ab8986a82f008791ae99cb315
chihabhajji <35853327+chihabhajji@users.noreply.github.com>
2021-11-11 02:27:12 +0100

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -7,5 +7,25 @@ node {
     withSonarQubeEnv() {
       sh "${mvn}/bin/mvn clean verify sonar:sonar"
     }
+    post {
+        always {
+            echo 'One way or another, I have finished'
+            deleteDir() /* clean up our workspace */
+        }
+        success {
+            echo 'I succeeded!'
+        }
+        unstable {
+            echo 'I am unstable :/'
+        }
+        failure {
+             mail to: 'team@example.com',
+             subject: "Failed Pipeline: ${currentBuild.fullDisplayName}",
+             body: "Something is wrong with ${env.BUILD_URL}"
+        }
+        changed {
+            echo 'Things were different before...'
+        }
+    }
   }
 }
