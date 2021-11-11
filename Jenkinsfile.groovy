0185a0c19f459a8089095b696cbe3d45671e4376
chihabhajji <35853327+chihabhajji@users.noreply.github.com>
2021-11-11 04:08:58 +0100

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,6 +3,25 @@ node {
   stage('SCM') {
     checkout scm
   }
+   stage('Build docker image') {
+    withMaven {
+        sh "${mvn}/bin/mvn spring-boot:build-image"
+    }
+    post {
+      always {
+        deleteDir() /* clean up our workspace */
+       }
+       success {
+             mail to: 'chihab.hajji@esprit.tn',
+             subject: "Successful Pipeline: ${currentBuild.fullDisplayName}",
+             body: "Pipeline ${env.BUILD_URL} built with success!"
+        }
+        failure {
+             mail to: 'chihab.hajji@esprit.tn',
+             subject: "Failed Pipeline: ${currentBuild.fullDisplayName}",
+             body: "Something is wrong with ${env.BUILD_URL}'s SonarQubeAnalysis"
+        }
+    }
   stage('SonarQube Analysis') {
     withSonarQubeEnv() {
       sh "${mvn}/bin/mvn clean org.sonarsource.scanner.maven:sonar-maven-plugin:RELEASE:sonar -DskipTests"
@@ -28,24 +47,6 @@ node {
      "${mvn}/bin/mvn -B org.jacoco:jacoco-maven-plugin:prepare-agent-integration failsafe:integration-test failsafe:verify"
       step([$class: 'JUnitResultArchiver', testResults: '**/target/failsafe-reports/TEST-*.xml'])
   }
-  stage('Build docker image') {
-    withMaven {
-        sh "${mvn}/bin/mvn spring-boot:build-image"
-    }
-    post {
-      always {
-        deleteDir() /* clean up our workspace */
-       }
-       success {
-             mail to: 'chihab.hajji@esprit.tn',
-             subject: "Successful Pipeline: ${currentBuild.fullDisplayName}",
-             body: "Pipeline ${env.BUILD_URL} built with success!"
-        }
-        failure {
-             mail to: 'chihab.hajji@esprit.tn',
-             subject: "Failed Pipeline: ${currentBuild.fullDisplayName}",
-             body: "Something is wrong with ${env.BUILD_URL}'s SonarQubeAnalysis"
-        }
-    }
+ 
   }
 }
