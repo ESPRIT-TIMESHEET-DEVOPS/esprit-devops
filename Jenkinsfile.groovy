cd51f0013b6ee67fca779d1ee5b978de107d3b63
chihabhajji <blackpang23@gmail.com>
2021-11-11 09:36:35 +0100

    ok

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,58 +1,63 @@
 node {
+    agent {
+        docker {
+            image 'maven:3.8.1-adoptopenjdk-11'
+            args '-v /root/.m2:/root/.m2'
+        }
+    }
+    environment {
+        DOCKERHUB_CREDENTIALS = credentials('docker-login')
+    }
     stage('SCM') {
         checkout scm
+    }
+    stage('Test') {
+        withMaven {
+            sh "mvn clean test"
+        }
     }
     stage('SonarQube Analysis') {
         def mvn = tool 'Default Maven';
         withSonarQubeEnv() {
-            sh "${mvn}/bin/mvn clean verify org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar"
+            sh "${mvn}/bin/mvn clean verify org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar -DskipTests"
+        }
+    }
+    stage('Build docker image') {
+        stages {
+            stage('Build') {
+                withMaven {
+                    sh "mvn spring-boot:build-image"
+                }
+            }
+            stage('Login') {
+                steps {
+                    sh 'echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin'
+                }
+            }
+            stage('Push') {
+                steps {
+                    sh 'docker push espritchihab/timesheet:1.0'
+                }
+            }
+        }
+    }
+    post{
+        always {
+            sh 'docker logout'
+            deleteDir()
+        }
+        failure {
+            mail to: 'chihab.hajji@esprit.tn',
+                    subject: "Failed Pipeline: ${currentBuild.fullDisplayName}",
+                    body: "Something is wrong with ${env.BUILD_URL}'s test"
         }
     }
 }
-//        stage('Build docker image') {
-//        stages {
-//
-//            stage('Build'){
-//                withMaven {
-//                    sh "mvn spring-boot:build-image"
-//                }
-//            }
-//            stage('Login') {
-//                steps {
-//                    sh 'echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin'
-//                }
-//            }
-//            stage('Push') {
-//                steps {
-//                    sh 'docker push espritchihab/timesheet:1.0'
-//                }
-//            }
-//        }
 //        post {
-//            always {
-//                sh 'docker logout'
-//            }
+
 //        }
 //    }
-//    stage('Test') {
-//        withMaven {
-//            sh "mvn clean test"
-//        }
-//        post {
-//            failure {
-//                mail to: 'chihab.hajji@esprit.tn',
-//                        subject: "Failed Pipeline: ${currentBuild.fullDisplayName}",
-//                        body: "Something is wrong with ${env.BUILD_URL}'s test"
-//            }
-//        }
-//    }
-//    stage('SonarQube Analysis') {
-//        withSonarQubeEnv() {
-//            withMaven {
-//                sh "mvn clean sonar:sonar -DskipTests"
-//            }
-//        }
-//    }
+
 //    stage('Deploy to Nexus') {
 //        withMaven {
 //            sh "mvn install deploy -DskipTests"
